cf0677b3d316a92e538b4b7ee44b880253c5775f
Ville Saarinen <ville.saarinen9@gmail.com>
2023-09-07 00:08:05 +0300

    Setup icon for default group

--- src/app/shared/pipes/lastMovingAverage.pipe.ts
+++ src/app/shared/pipes/lastMovingAverage.pipe.ts
@@ -3,7 +3,10 @@ import { lastMovingAverage } from 'src/app/utils/lastMovingAverage';
 
 @Pipe({ name: 'lastMovingAve' })
 export class LastMovingAveragePipe implements PipeTransform {
-  transform(array: any, field: string): any {
-    return lastMovingAverage(array.map((a: any) => a[field]));
+  transform(array: any, field: string, movingLength: number = 3): any {
+    return lastMovingAverage(
+      array.map((a: any) => a[field]),
+      movingLength
+    );
   }
 }
